358f87331fc63757dddc6f8ea9da6b628996553e
rosemary.charnley@smartbear.com <rosemary.charnley@smartbear.com>
2026-08-12 13:54:05 -0400

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxODE2NA/spectral.js?branch_slug=main&token=ef75b369-c523-43d6-b95b-c67ce3717f45";
+export default { extends: ruleset };
